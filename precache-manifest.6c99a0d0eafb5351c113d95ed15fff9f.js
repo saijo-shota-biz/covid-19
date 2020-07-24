dc34354594bedc462213ba8d093d2df319dc31a3
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-07-25 02:29:08 +0900

    Updates

---
+++ precache-manifest.6c99a0d0eafb5351c113d95ed15fff9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856812c8034cd49ade9ff2ac52e345b8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "0b9ec70e7ea60b44cd85",
+    "url": "/covid-19/static/css/main.e6b24f4d.chunk.css"
+  },
+  {
+    "revision": "0d668181557c83bbb219",
+    "url": "/covid-19/static/js/2.624e5d4d.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid-19/static/js/2.624e5d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9ec70e7ea60b44cd85",
+    "url": "/covid-19/static/js/main.0f9794ed.chunk.js"
+  },
+  {
+    "revision": "44f07a1a748e4a65aa62",
+    "url": "/covid-19/static/js/runtime-main.0df86206.js"
+  }
+]);
